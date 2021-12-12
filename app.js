5085a0c2572059c0c14033915d54b9d4f42ffda5
amartya-rs <saha.amartya@outlook.com>
2021-12-13 00:34:28 +0530

    feat: visibility on button click

--- app.js
+++ app.js
@@ -2,25 +2,41 @@ var button = document.querySelector("#button");
 var bill = document.querySelector("#bill");
 var cash = document.querySelector("#cash");
 var table = document.querySelectorAll(".no-of-notes");
+var nextButton = document.querySelector("#next-btn");
+var divSec = document.querySelector(".div-sec");
+var secTwo = document.querySelector(".sec2");
+
 
 button.addEventListener("click", clickHandler);
+nextButton.addEventListener("click", nextSection);
 
+function nextSection(){
+    var userBill = 0;
+    var userBill = Number(bill.value);
+
+    if (userBill>0) {
+        divSec.style.visibility = "visible";
+    }
+    else {
+        alert("Invalid bill amount");
+    }
+    
+}
 
 
 function clickHandler (){
-    var userBill = 0;
+    
     var userCash = 0;
     var userBill = Number(bill.value);
     var userCash = Number(cash.value);
     var noteNo = 0;
    
-     
-    console.log(userBill, userCash);
+    
     if (userBill>0) {
         if (userCash>=userBill) {
             var notes = [2000, 500, 100, 20, 10, 5, 1];
             var change = userCash-userBill;
-    
+            secTwo.style.visibility = "visible";
             for (i=0; i<7; i++){
 
                 noteNo= Math.trunc(change/notes[i]);
